86b88d8245524e2318660da961343da018810001
Mizanali Panjwani <mizanalip786@gmail.com>
2022-01-16 23:37:31 +0530

    Deployment setup

--- server.js
+++ server.js
@@ -17,14 +17,6 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-//serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Serving on port ${PORT}`);
